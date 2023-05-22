ec1d0a9f761bdd0aeb654ed52f06739103c33111
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-05-22 12:02:56 +0200

    sum of n with recursion (and loop)

---
+++ sum_n_recursion.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+
+// Using Recursion
+int sum(int n) {
+
+    if (n == 0) 
+        return 0;
+    return sum(n-1) + n;
+}
+
+// using for loop
+int Isum(int n) {
+
+    int s = 0;      // to store sum
+    int i;          // for counter
+    for (i=1; i <= n; i++)
+        s=s+i;
+    return s;
+
+} 
+
+int main() {
+
+    int r, s;
+
+    r = sum(5);
+    s = Isum(5);
+    std::cout << r << std::endl;
+    std::cout << s << std::endl;
+    
+    return 0;
+}
